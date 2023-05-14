fc8740659b97912f33decf322ffbb342650ab07e
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-14 16:04:55 +0600

    verify token and access booking data

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT=(req,res,next)=>{
+  console.log(req.headers.authorization)
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'Unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+    if(error){
+      return res.status(401).send({error:true,message:'Unauthorized access'})
+    }
+    req.decoded=decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -42,7 +58,7 @@ app.post('/jwt',(req,res)=>{
   const user =req.body;
   console.log(user);
   const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-    expiresIn:'1h'
+    expiresIn:'1hr'
   });
   res.send({token})
 })
@@ -70,8 +86,13 @@ app.get('/services/:id', async(req,res)=>{
 // bookings
 
 // get some data
-app.get('/bookings',async(req,res)=>{
-    console.log(req.query.email)
+app.get('/bookings',verifyJWT, async(req,res)=>{
+  const decoded = req.decoded;
+  console.log('come back after verify',decoded);
+if(decoded.email !== req.query.email){
+  return res.status(403).send({error:true,message:'Forbiden'})
+}
+    // console.log(req.query.email)
     let query={}
     if(req.query?.email){
         query={email:req.query.email}
