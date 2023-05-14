745280079743f3596a5c7b17c6504c33f7dd519c
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-14 18:20:51 +0600

    solved deploy error

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/jwt',(req,res)=>{
   const user =req.body;
   console.log(user);
   const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-    expiresIn:'1hr'
+    expiresIn:'1h'
   });
   res.send({token})
 })
